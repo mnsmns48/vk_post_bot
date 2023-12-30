88105eaec8ecea7162d00af72ea3a977d0e86d69
cifrotech <cifrotechmobile@inbox.ru>
2023-12-30 18:21:23 +0300

    Bot side is ready

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -3,20 +3,20 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 main_kb = InlineKeyboardBuilder()
 main_kb.add(InlineKeyboardButton(
-    text='Предложить пост',
+    text='Предложить пост📝',
     callback_data='suggest')
 )
 main_kb.add(InlineKeyboardButton(
-    text='Написать админу',
+    text='Написать админу👨🏻‍💼',
     callback_data='to_admin')
 )
 
 public = InlineKeyboardBuilder()
 public.add(InlineKeyboardButton(
-    text='Ошибка! Начать заново',
+    text='Изменить пост🛠️',
     callback_data='again')
 )
 public.add(InlineKeyboardButton(
-    text='Опубликовать пост',
+    text='Опубликовать пост🚀',
     callback_data='public')
 )
--- bot/user_handlers.py
+++ bot/user_handlers.py
@@ -32,7 +32,7 @@ async def start(m: Message):
 
 
 async def suggest_post_callback(c: CallbackQuery, state=FSMContext):
-    await c.answer(text='Выбрано')
+    await c.answer(text='📣📣📣📣📣📣📣📣📣📣📣📣')
     await state.set_state(ListenUser.suggest_)
     await c.message.answer(text='Пиши текст, отправляй вложения\n\n'
                                 'ВАЖНО!\n'
@@ -40,11 +40,12 @@ async def suggest_post_callback(c: CallbackQuery, state=FSMContext):
                                 'Если пост содержит фото и/или видеофайл, сначала добавьте эти файлы, '
                                 'а текст прикрепите, как подпись к ним\n'
                                 'Допускается до 10 медиафайлов\n\n'
+                                'Не забудь указать контакт для связи, если это нужно\n\n'
                                 'Жду пост....')
 
 
 async def to_admin_callback(c: CallbackQuery, state=FSMContext):
-    await c.answer(text='Выбрано')
+    await c.answer(text='Слушаю вас.....')
     await state.set_state(ListenUser.to_admin_)
     await c.message.answer(text='АДМИН этого канала готов выслушать все предложения и пожелания')
 
@@ -52,41 +53,46 @@ async def to_admin_callback(c: CallbackQuery, state=FSMContext):
 async def to_admin(m: Message, state: FSMContext):
     text = f'Сообщение админу\nОт {m.from_user.full_name} {m.from_user.username}\n{m.text}'
     await bot.send_message(chat_id=hv.tg_bot_admin_id[0], text=text)
-    await m.answer('Сообщение админу отправлено', reply_markup=main_kb.as_markup())
+    await m.answer('Сообщение админу отправлено\n'
+                   'Он скоро прочтёт его. До свидания', reply_markup=main_kb.as_markup())
     await state.clear()
 
 
-async def suggest_post(m: Message, state: FSMContext, album: list[Message] = None):
+async def suggest_post(m: Message, state: FSMContext, album: list[Message] = None) -> Message:
     answer_text = 'Твой пост будет выглядит так:\n'
     if m.content_type == ContentType.TEXT:
-        await m.answer(f"{answer_text}\n\n{m.text}")
-        await m.answer("Публикуем?", reply_markup=public.as_markup())
-        return await state.clear()
+        text_line = f"{m.text}\n\n{m.from_user.full_name}\n@{m.from_user.username}"
+        await m.answer(f"{answer_text}\n\n{text_line}")
+        await state.update_data(only_text=text_line)
+        return await m.answer("Публикуем? Ожидаю ответ...", reply_markup=public.as_markup())
     album_builder = MediaGroupBuilder(
-        caption=m.caption
+        caption=f"{m.caption}\n\n{m.from_user.full_name}\n@{m.from_user.username}"
     )
     response = receive_attach(album=album_builder, m=m)
     if album:
         for i in range(1, len(album)):
             response.build().append(receive_attach(album=response, m=album[i]))
-    await m.answer(f"{answer_text}\n\n{m.text if m.text else ' '}")
+    await m.answer(f"{answer_text}")
     await m.answer_media_group(response.build())
-    await state.update_data(post=response)
-    await m.answer("Публикуем?", reply_markup=public.as_markup())
+    await state.update_data(media_group=response)
+    return await m.answer("Публикуем? Ожидаю ответ...", reply_markup=public.as_markup())
 
 
 async def callback_handler_public(c: CallbackQuery, state=FSMContext):
-    await c.answer(text='Выбор сделан')
-    a = await state.get_data()
+    await c.answer(text='Пост отправлен администратору на модерацию')
+    data_in_state = await state.get_data()
     await bot.send_message(chat_id=hv.tg_bot_admin_id[0], text='!!!!!!!!!!Пост!!!!!!\n')
-    await bot.send_media_group(chat_id=hv.tg_bot_admin_id[0], media=a.get('post').build())
-    await c.message.answer('Отправлено. Ожидайте публикации', reply_markup=main_kb.as_markup())
-    await state.clear()
+    if data_in_state.get('only_text'):
+        await bot.send_message(chat_id=hv.tg_bot_admin_id[0], text=data_in_state.get('only_text'))
+    else:
+        await bot.send_media_group(chat_id=hv.tg_bot_admin_id[0], media=data_in_state.get('media_group').build())
+    await c.message.answer('Отправлено. Ожидайте публикации')
+    return await state.clear()
 
 
 async def callback_handler_again(c: CallbackQuery, state=FSMContext):
     await c.answer('Отмена')
-    await c.message.answer('Попробуйте начать заново', reply_markup=main_kb.as_markup())
+    await c.message.answer('Вы отменили ваш пост, добавьте его заново', reply_markup=main_kb.as_markup())
     await state.clear()
 
 
